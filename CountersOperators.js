56e8ea164ce8c960e143f84740e6f74cf409678c
CygnusStarion <54466514+CygnusStarion@users.noreply.github.com>
2019-10-18 19:06:17 -0500

    Add files via upload

---
+++ CountersOperators.js
@@ -0,0 +1,22 @@
+// Write a program that sums the numbers from 1 to 10.
+
+
+//let counter = 1;
+//let sum = 0;for (let i = 0; i <= 10; i += 1) {
+    for (let i = 0; i <= 10; i += 1) {
+        sum += i; // sum = sum +i;
+
+    }
+
+
+
+
+// '<=' is COMPARISON OPERATOR
+//while (counter <= 10) {
+ //   sum += counter; //sum = sum + counter;
+
+    // '+=' is an ASSIGNMENT OPERATORcounter += 1; // counter = counter +1;
+
+//}
+
+console.log(sum);
